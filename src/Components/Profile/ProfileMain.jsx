4c6fe782726dc24297f43f48bb161b97a98feecc
Caleb Ochai <calebochai09@gmail.com>
2023-04-30 08:25:12 -0700

    Modified the showMore state

--- src/Components/Profile/ProfileMain.jsx
+++ src/Components/Profile/ProfileMain.jsx
@@ -25,18 +25,18 @@ const ProfileMain = () => {
       )}
 
       <div
-        className="hidden xl:block hover:bg-gray-200 rounded-md my-4"
+        className="block xl:hidden hover:bg-gray-200 rounded-md my-4"
         onClick={() => setShowMore((prevState) => !prevState)}
       >
         {showMore ? (
-          <div className="hidden xl:flex items-center gap-1 justify-center py-1">
+          <div className="xl:hidden flex items-center gap-1 justify-center py-1">
             <span className="cursor-pointer text-sm text-links font-medium">
               Show less
             </span>
             <FaChevronUp className="text-links" />
           </div>
         ) : (
-          <div className="hidden xl:flex items-center gap-1 justify-center py-1">
+          <div className="xl:hidden flex items-center gap-1 justify-center py-1">
             <span className="cursor-pointer text-sm text-links font-medium">
               Show more
             </span>
